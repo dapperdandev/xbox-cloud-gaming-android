9a673874ffc5ba8efc2f1d96431d025521812be6
Daniel Breen <daniel.breen@tackle.io>
2022-11-03 16:23:43 -0500

    Initial Commit

---
+++ app/src/main/java/com/dapperdandev/xbox/unofficial/DelegationService.java
@@ -0,0 +1,14 @@
+package com.dapperdandev.xbox.unofficial;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
